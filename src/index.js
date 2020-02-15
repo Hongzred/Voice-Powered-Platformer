02d1a7996395b9e511a56e89dbec109eba1f04f8
hongzhi pan <victoryhongzhi@gmail.com>
2020-02-15 18:22:17 -0500

    refactor and removed unused code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
 import config from './config/config';
 import GameScene from './scenes/GameScene';
 
@@ -13,21 +12,4 @@ class Game extends Phaser.Game {
 
 window.onload = function () {
   window.game = new Game();
-}
-
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
 }
