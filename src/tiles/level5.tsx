4779a1e012b346f8bfb01a8e05da151bf3659225
hongzhi pan <victoryhongzhi@gmail.com>
2020-04-19 18:28:28 -0400

    level 6

--- src/tiles/level5.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="level5" tilewidth="8" tileheight="8" tilecount="2652" columns="102">
- <image source="../assets/level5.png" width="816" height="208"/>
-</tileset>
