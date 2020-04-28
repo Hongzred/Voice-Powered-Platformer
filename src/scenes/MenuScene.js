4459499ecbdd1b62c9141472898ffa6b7ba7554c
hongzhi pan <victoryhongzhi@gmail.com>
2020-04-28 14:14:19 -0400

    added logo to menu page

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -5,6 +5,7 @@ import LevelIntro from "./LevelIntro";
 import skyImg from "../assets/sky.png";
 import player from "../assets/car.png";
 import menuMusic from "../assets/menu_music.mp3";
+import vpp_logo from "../assets/vpp-logo.png";
 export default class MenuScene extends Phaser.Scene {
     constructor () {
         super({key: 'Menu', active: false});
@@ -12,6 +13,7 @@ export default class MenuScene extends Phaser.Scene {
 
     preload() {
         this.load.image("sky", skyImg);
+        let vpp = this.load.image("vpp_logo", vpp_logo);
         this.load.image('player', player);
         let loadingBar = this.add.graphics({
             fillStyle: {
@@ -27,6 +29,7 @@ export default class MenuScene extends Phaser.Scene {
     }
     create() {
         this.add.image(400, 300, 'sky');
+        this.add.image(400, 200, 'vpp_logo').setDisplaySize(200,200);
         this.lights.enable();
 
         this.lights.addLight(300, 300, 300, 0xff0000, 1);
@@ -38,10 +41,10 @@ export default class MenuScene extends Phaser.Scene {
         })
         let x = config.centerX;
         let y = config.centerY;
-        const title = this.add.text(x, y - 50, 'V.P.P', {
+        const title = this.add.text(x - 15, y + 50, 'V.P.P', {
             fontSize: "32px",
           });
-        const text = this.add.text(x, y + 50, "Start", {
+        const text = this.add.text(x - 15, y + 150, "Start", {
             fontSize: "32px",
           });
         this.createMouseInput();
